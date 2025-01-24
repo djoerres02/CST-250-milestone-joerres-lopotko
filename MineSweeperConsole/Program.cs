d64b37736bfa6249e8f1374c45a1dc8acf200df5
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2025-01-24 12:36:06 -0700

    implementing minesweeper class library

--- MineSweeperConsole/Program.cs
+++ MineSweeperConsole/Program.cs
@@ -1,6 +1,9 @@
-﻿static void Main(string[] args)
+﻿using MineSweeperClasses;
+
+static void Main(string[] args)
 {
     Console.WriteLine("Hello, welcome to Minesweeper");
     // size 10 and difficulty 0.1
     Board board = new Board(10, 0.1f);
 }
+
