791b686346fc0406b8951fcc1cde09f87115b464
djoerres02 <djoerres02@gmail.com>
2025-01-24 12:17:02 -0700

    Cell class shell implemented

--- MineSweeperClasses/Cell.cs
+++ MineSweeperClasses/Cell.cs
@@ -8,5 +8,24 @@ namespace MineSweeperClasses
 {
     public class Cell
     {
+        //Field Variables
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public bool IsVisited { get; set; }
+        public bool IsBomb { get; set; }
+        public bool IsFlagged { get; set; }
+        public bool HasSpecialReward {  get; set; } //use hint ability
+        
+
+        //Parameterized Constructor
+        public Cell(int row, int column)
+        {
+            Row = row;
+            Column = column;
+            IsVisited = false;
+            IsBomb = false;
+            IsFlagged = false;
+            HasSpecialReward = false;
+        }
     }
 }
