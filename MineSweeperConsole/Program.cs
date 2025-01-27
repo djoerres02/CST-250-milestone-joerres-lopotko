5b183fd40fb7639a7c78956a6521d9160c56c050
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2025-01-26 18:46:33 -0700

    Merged code from main

--- MineSweeperConsole/Program.cs
+++ MineSweeperConsole/Program.cs
@@ -4,6 +4,6 @@ static void Main(string[] args)
 {
     Console.WriteLine("Hello, welcome to Minesweeper");
     // size 10 and difficulty 0.1
-    Board board = new Board(10, 0.1f);
+    //Board board = new Board(10, 0.1f);
 }
 
