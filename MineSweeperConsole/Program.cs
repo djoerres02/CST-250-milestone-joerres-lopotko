100bf64b5fb9d22bccb756364858b7f546068587
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2025-01-26 19:00:18 -0700

    Added PrintAnswers to Main.

--- MineSweeperConsole/Program.cs
+++ MineSweeperConsole/Program.cs
@@ -4,6 +4,13 @@ static void Main(string[] args)
 {
     Console.WriteLine("Hello, welcome to Minesweeper");
     // size 10 and difficulty 0.1
-    Board board = new Board(10, 10);
+    Board board = new Board(10, 0.1f);
+    Console.WriteLine("Here is the answer key for the first board");
+    PrintAnswers(board);
+
+    // size 15 and difficulty 0.15
+    board = new Board(15, 0.15f);
+    Console.WriteLine("Here is the answer key for the second board");
+    PrintAnswers(board);
 }
 
