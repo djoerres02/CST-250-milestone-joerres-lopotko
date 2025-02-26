4c6bd3229b43ef342471db98cb629f36b319b8a0
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2025-02-26 13:06:11 -0700

    Commit FrmGameSettings Names

--- MineSweeperGUI/FrmGameSettings.Designer.cs
+++ MineSweeperGUI/FrmGameSettings.Designer.cs
@@ -28,28 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            trackBar1 = new TrackBar();
-            trackBar2 = new TrackBar();
+            trkSize = new TrackBar();
+            trkBombs = new TrackBar();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            grpLevel = new GroupBox();
+            radEasy = new RadioButton();
+            radMedium = new RadioButton();
+            radDifficult = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)trkSize).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trkBombs).BeginInit();
+            grpLevel.SuspendLayout();
             SuspendLayout();
             // 
-            // trackBar1
+            // trkSize
             // 
-            trackBar1.Location = new Point(12, 57);
-            trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(295, 45);
-            trackBar1.TabIndex = 0;
+            trkSize.Location = new Point(12, 57);
+            trkSize.Name = "trkSize";
+            trkSize.Size = new Size(295, 45);
+            trkSize.TabIndex = 0;
             // 
-            // trackBar2
+            // trkBombs
             // 
-            trackBar2.Location = new Point(12, 167);
-            trackBar2.Name = "trackBar2";
-            trackBar2.Size = new Size(295, 45);
-            trackBar2.TabIndex = 0;
+            trkBombs.Location = new Point(12, 167);
+            trkBombs.Name = "trkBombs";
+            trkBombs.Size = new Size(295, 45);
+            trkBombs.TabIndex = 0;
             // 
             // label1
             // 
@@ -78,30 +83,82 @@
             label3.TabIndex = 2;
             label3.Text = "Play Minesweeper";
             // 
+            // grpLevel
+            // 
+            grpLevel.Controls.Add(radDifficult);
+            grpLevel.Controls.Add(radMedium);
+            grpLevel.Controls.Add(radEasy);
+            grpLevel.Location = new Point(53, 233);
+            grpLevel.Name = "grpLevel";
+            grpLevel.Size = new Size(200, 100);
+            grpLevel.TabIndex = 3;
+            grpLevel.TabStop = false;
+            grpLevel.Text = "Choose a level";
+            // 
+            // radEasy
+            // 
+            radEasy.AutoSize = true;
+            radEasy.Location = new Point(6, 22);
+            radEasy.Name = "radEasy";
+            radEasy.Size = new Size(48, 19);
+            radEasy.TabIndex = 0;
+            radEasy.TabStop = true;
+            radEasy.Text = "Easy";
+            radEasy.UseVisualStyleBackColor = true;
+            // 
+            // radMedium
+            // 
+            radMedium.AutoSize = true;
+            radMedium.Location = new Point(6, 47);
+            radMedium.Name = "radMedium";
+            radMedium.Size = new Size(70, 19);
+            radMedium.TabIndex = 0;
+            radMedium.TabStop = true;
+            radMedium.Text = "Medium";
+            radMedium.UseVisualStyleBackColor = true;
+            // 
+            // radDifficult
+            // 
+            radDifficult.AutoSize = true;
+            radDifficult.Location = new Point(6, 75);
+            radDifficult.Name = "radDifficult";
+            radDifficult.Size = new Size(67, 19);
+            radDifficult.TabIndex = 0;
+            radDifficult.TabStop = true;
+            radDifficult.Text = "Difficult";
+            radDifficult.UseVisualStyleBackColor = true;
+            // 
             // FrmGameSettings
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(323, 230);
+            ClientSize = new Size(323, 345);
+            Controls.Add(grpLevel);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(trackBar2);
-            Controls.Add(trackBar1);
+            Controls.Add(trkBombs);
+            Controls.Add(trkSize);
             Name = "FrmGameSettings";
-            Text = "FrmGameSettings";
-            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            Text = "Start a New Game";
+            ((System.ComponentModel.ISupportInitialize)trkSize).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trkBombs).EndInit();
+            grpLevel.ResumeLayout(false);
+            grpLevel.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private TrackBar trackBar1;
-        private TrackBar trackBar2;
+        private TrackBar trkSize;
+        private TrackBar trkBombs;
         private Label label1;
         private Label label2;
         private Label label3;
+        private GroupBox grpLevel;
+        private RadioButton radDifficult;
+        private RadioButton radMedium;
+        private RadioButton radEasy;
     }
 }
