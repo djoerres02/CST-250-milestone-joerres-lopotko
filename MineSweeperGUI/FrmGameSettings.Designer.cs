a01228d13ff13fc75bf6720f0b4727b7149a0ecf
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2025-02-26 12:58:11 -0700

    Commit FrmGameSettings Form Controls

--- MineSweeperGUI/FrmGameSettings.Designer.cs
+++ MineSweeperGUI/FrmGameSettings.Designer.cs
@@ -29,7 +29,12 @@
         private void InitializeComponent()
         {
             trackBar1 = new TrackBar();
+            trackBar2 = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
             SuspendLayout();
             // 
             // trackBar1
@@ -39,15 +44,54 @@
             trackBar1.Size = new Size(295, 45);
             trackBar1.TabIndex = 0;
             // 
+            // trackBar2
+            // 
+            trackBar2.Location = new Point(12, 167);
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(295, 45);
+            trackBar2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(27, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Size";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 149);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Percent Bombs";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Play Minesweeper";
+            // 
             // FrmGameSettings
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(323, 230);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(trackBar2);
             Controls.Add(trackBar1);
             Name = "FrmGameSettings";
             Text = "FrmGameSettings";
             ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +99,9 @@
         #endregion
 
         private TrackBar trackBar1;
+        private TrackBar trackBar2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
