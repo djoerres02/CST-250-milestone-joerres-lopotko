b1712b9e974f071d29a41cb20e7e4c7df0e7d72b
djoerres02 <djoerres02@gmail.com>
2025-03-08 02:33:27 -0700

    added ResetBoard method and UpdateButtons method

--- MineSweeperGUI/FrmMineSweeper.Designer.cs
+++ MineSweeperGUI/FrmMineSweeper.Designer.cs
@@ -36,75 +36,93 @@
             lblScore = new Label();
             btnRestart = new Button();
             tmrStopWatch = new System.Windows.Forms.Timer(components);
+            btnUseReward = new Button();
             SuspendLayout();
             // 
             // pnlGame
             // 
-            pnlGame.Location = new Point(82, 12);
+            pnlGame.Location = new Point(94, 16);
+            pnlGame.Margin = new Padding(3, 4, 3, 4);
             pnlGame.Name = "pnlGame";
-            pnlGame.Size = new Size(400, 400);
+            pnlGame.Size = new Size(457, 533);
             pnlGame.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(570, 64);
+            label1.Location = new Point(651, 85);
             label1.Name = "label1";
-            label1.Size = new Size(64, 15);
+            label1.Size = new Size(80, 20);
             label1.TabIndex = 0;
             label1.Text = "Start Time:";
             // 
             // lblTime
             // 
             lblTime.AutoSize = true;
-            lblTime.Location = new Point(570, 90);
+            lblTime.Location = new Point(651, 120);
             lblTime.Name = "lblTime";
-            lblTime.Size = new Size(34, 15);
+            lblTime.Size = new Size(42, 20);
             lblTime.TabIndex = 1;
             lblTime.Text = "Time";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(570, 134);
+            label2.Location = new Point(651, 179);
             label2.Name = "label2";
-            label2.Size = new Size(39, 15);
+            label2.Size = new Size(49, 20);
             label2.TabIndex = 0;
             label2.Text = "Score:";
             // 
             // lblScore
             // 
             lblScore.AutoSize = true;
-            lblScore.Location = new Point(570, 160);
+            lblScore.Location = new Point(651, 213);
             lblScore.Name = "lblScore";
-            lblScore.Size = new Size(13, 15);
+            lblScore.Size = new Size(17, 20);
             lblScore.TabIndex = 1;
             lblScore.Text = "0";
             // 
             // btnRestart
             // 
-            btnRestart.Location = new Point(591, 206);
+            btnRestart.Location = new Point(651, 335);
+            btnRestart.Margin = new Padding(3, 4, 3, 4);
             btnRestart.Name = "btnRestart";
-            btnRestart.Size = new Size(75, 23);
+            btnRestart.Size = new Size(86, 31);
             btnRestart.TabIndex = 2;
             btnRestart.Text = "Restart";
             btnRestart.UseVisualStyleBackColor = true;
+            btnRestart.Click += BtnRestartClickEH;
             // 
             // tmrStopWatch
             // 
             tmrStopWatch.Tick += TmrStopwatchTickEH;
             // 
+            // btnUseReward
+            // 
+            btnUseReward.AutoSize = true;
+            btnUseReward.Location = new Point(651, 281);
+            btnUseReward.Margin = new Padding(3, 4, 3, 4);
+            btnUseReward.Name = "btnUseReward";
+            btnUseReward.Size = new Size(102, 31);
+            btnUseReward.TabIndex = 3;
+            btnUseReward.Text = "Use Reward";
+            btnUseReward.UseVisualStyleBackColor = true;
+            btnUseReward.Visible = false;
+            // 
             // FrmMineSweeper
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(707, 450);
+            ClientSize = new Size(808, 600);
+            Controls.Add(btnUseReward);
             Controls.Add(btnRestart);
             Controls.Add(lblScore);
             Controls.Add(label2);
             Controls.Add(lblTime);
             Controls.Add(label1);
             Controls.Add(pnlGame);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmMineSweeper";
             Text = "Minesweeper";
             ResumeLayout(false);
@@ -120,5 +138,6 @@
         private Label lblScore;
         private Button btnRestart;
         private System.Windows.Forms.Timer tmrStopWatch;
+        private Button btnUseReward;
     }
 }
--- MineSweeperGUI/FrmMineSweeper.cs
+++ MineSweeperGUI/FrmMineSweeper.cs
@@ -14,13 +14,17 @@ namespace MineSweeperGUI
     {
         // Class level variables
         private Board board;
+        private BoardLogic boardLogic;
         TimeSpan timeSpan = new TimeSpan();
         bool gameOn = false;
+        Button[,] buttons;
 
         public FrmMineSweeper(Board gameBoard)
         {
             InitializeComponent();
             board = gameBoard;
+            buttons = new Button[board.Size, board.Size];
+            boardLogic = new BoardLogic(board);
             // Call method to set up buttons
             SetUpButtons();
             // Setup the game's time label
@@ -46,7 +50,6 @@ namespace MineSweeperGUI
                 // Set up each individual button
                 for (int col = 0; col < board.Size; col++)
                 {
-                    Button[,] buttons = new Button[board.Size, board.Size];
                     // Create a new button in the 2D array
                     buttons[row, col] = new Button();
                     // Set the size of the button
@@ -59,9 +62,9 @@ namespace MineSweeperGUI
                     // Set button color
                     buttons[row, col].BackColor = SystemColors.Control;
                     // Assign cell click event handler to each button
-                    buttons[row, col].Click += BtnCellClickEH;
+                    buttons[row, col].MouseDown += BtnCellMouseDownEH;
                     // Tag a point to each button
-                    buttons[row, col].Tag = new Point(row, col);
+                    buttons[row, col].Tag = new Point(col, row);//col is x value, row is y value
                     // Add the buttons to the game panel
                     pnlGame.Controls.Add(buttons[row, col]);
                 }
@@ -71,10 +74,11 @@ namespace MineSweeperGUI
         /// <summary>
         /// Click event handler for buttons
         /// Soon as the first button is clicked, the game timer will start.
+        /// tied to mouseclick 
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void BtnCellClickEH(object sender, EventArgs e)
+        private void BtnCellMouseDownEH(object sender, MouseEventArgs e)
         {
             // If the game hasn't been started yet
             if (!gameOn)
@@ -89,11 +93,72 @@ namespace MineSweeperGUI
             // Retreive Tag property of button
             Point point = (Point)button.Tag;
             // Extract the row and col values from the Point object
-            int row = point.X;
-            int col = point.Y;
+            int row = point.Y;
+            int col = point.X;
 
             // Display choice to user (Testing purposes)
-            MessageBox.Show($"Cell {row},{col} has been selected");
+            //MessageBox.Show($"Cell {row},{col} has been selected");
+
+            //if the click is a right mouseclick, flag or unflag the clicked cell
+            if(e.Button  == MouseButtons.Right)
+            {
+                if (board.Cells[row, col].IsFlagged)
+                {
+                    board.Cells[row, col].IsFlagged = false;
+                }
+                else
+                {
+                    board.Cells[row, col].IsFlagged = true;
+                }
+                UpdateButtons();
+            }
+            else if (!board.Cells[row, col].IsFlagged)
+            {
+                //call flood fill on cell if cell is empty
+                if (board.Cells[row, col].NumberOfBombNeighbors < 1)
+                {
+                    board = boardLogic.FloodFill(board, row, col);
+                }
+                // Mark the cell as visited
+                board.Cells[row, col].IsVisited = true;
+
+                //give the player a use of the reward if found
+                if (board.Cells[row, col].HasSpecialReward)
+                {
+                    board.RewardsRemaining++;
+                }
+
+                //After move is made, update the board
+                UpdateButtons();
+
+                // Determine if the game is over
+                Board.GameStatus state = boardLogic.DetermineGameState();
+
+                // If user won the game
+                if (state == Board.GameStatus.Won)
+                {
+                    // Give celebratory message
+                    MessageBox.Show("Congratulations! You've won the game!");
+
+                    // Set gameOver to true, breaking gameplay loop
+                    gameOn = false;
+                }
+                // If user lost the game
+                else if (state == Board.GameStatus.Lost)
+                {
+                    // Give game over message
+                    MessageBox.Show("Sorry, you hit a bomb. Game over!");
+
+                    // Set gameOver to true, breaking gameplay loop
+                    gameOn = false;
+                }
+
+                //When game is over, exit the application
+                if (gameOn == false)
+                {
+                    Application.Exit();
+                }
+            }
 
         }
 
@@ -105,6 +170,94 @@ namespace MineSweeperGUI
         private void TmrStopwatchTickEH(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// updates grid of buttons to refllect the board
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        private void UpdateButtons()
+        {
+            //walkthrough the grid of buttons, updating each one with the corresponding cells contents
+            for (int row = 0; row < board.Size; row++)
+            {
+                for (int col = 0; col < board.Size; col++)
+                {
+                    //set the contents of the button depending on what the corresponding cell if visited or flagged
+                    if (board.Cells[row, col].IsVisited)
+                    { 
+                        if (board.Cells[row, col].IsBomb)
+                        {
+                            buttons[row, col].Text = "B";
+                        }
+                        else if (board.Cells[row, col].HasSpecialReward)
+                        {
+                            buttons[row, col].Text = "R";
+                            buttons[row, col].Enabled = false;
+                        }
+                        else if (board.Cells[row, col].NumberOfBombNeighbors > 0)
+                        {
+                            buttons[row, col].Text = "" + board.Cells[row, col].NumberOfBombNeighbors;
+                            buttons[row, col].Enabled = false;
+                        }
+                        else
+                        {
+                            buttons[row, col].Enabled = false;
+                        }
+                    }
+                    else if (board.Cells[row, col].IsFlagged)
+                    {
+                        buttons[row, col].Text = "F";
+                    }
+                    //if cell isn't visited or flagged, leave it empty
+                    else
+                    {
+                        buttons[row, col].Text = "";
+                    }
+                }
+            }
+            //update visibility of reward button if user has a reward to use.
+            if(board.RewardsRemaining > 0)
+            {
+                btnUseReward.Visible = true;
+            }
+            else
+            {
+                btnUseReward.Visible = false;
+            }
+        }
+
+        /// <summary>
+        /// resets the board with new bomb placements
+        /// </summary>
+        private void ResetBoard()
+        {
+            //get the size and difficulty from the previous board to instantiate a new board
+            int size = board.Size;
+            int difficulty = board.Difficulty;
+            board = new Board(size, difficulty);
+            //instantiate a new boardLogic object with the new board
+            //This will call the board setup methods again, "reseting" the board
+            boardLogic = new BoardLogic(board);
+            //re-enable all the buttons
+            foreach(Button btn in pnlGame.Controls)
+            {
+                btn.Enabled = true;
+                btn.Text = "";
+            }
+            //finally, update the board
+            UpdateButtons();
+        }
+
+        /// <summary>
+        /// Resets the board completely wiping the previous game and starting new
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnRestartClickEH(object sender, EventArgs e)
+        {
+            ResetBoard();
         }
     }
 }
