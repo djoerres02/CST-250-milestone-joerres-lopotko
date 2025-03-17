1d1463ed582d2179f74d6a8ad2d055b86ef64a81
Joseph Lopotko <142354212+mrjoebling@users.noreply.github.com>
2025-03-17 14:52:51 -0700

    commit gamestat class

---
+++ MineSweeperClasses/Models/GameStat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeperClasses.Models
+{
+    public class GameStat
+    {
+        private int id;
+        private string name;
+        private int score;
+        private TimeSpan gameTime;
+    }
+}
